bbc176c5bd89ee0570698148ffd5fa6025bc8372
Alessandro Astone <alessandro.astone@hotmail.com>
2018-08-07 21:25:27 +0200

    msm8996-common: recovery: updates for Pie
    Change-Id: I480386b8ecbb9e308e047cc743b4da087e9b44a3

--- recovery/Android.mk
+++ recovery/Android.mk
@@ -1,17 +1,24 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-
 ifneq ($(TARGET_PLATFORM_DEVICE_BASE),)
 LOCAL_CFLAGS += -DUSES_BOOTDEVICE_PATH
 endif
 
 LOCAL_C_INCLUDES := \
-    $(call project-path-for,recovery) \
-    $(call project-path-for,recovery)/updater/include \
+    $(TARGET_OUT_HEADERS)/edify \
+    $(TARGET_OUT_HEADERS)/otautil \
+    $(TARGET_OUT_HEADERS)/updater \
     bootable/recovery \
     system/core/base/include
 LOCAL_SRC_FILES := recovery_updater.cpp
 LOCAL_MODULE := librecovery_updater_msm8996
+
 LOCAL_MODULE_TAGS := eng
+
+LOCAL_STATIC_LIBRARIES := \
+    libedify \
+    libotautil \
+    libupdater
+
 include $(BUILD_STATIC_LIBRARY)
